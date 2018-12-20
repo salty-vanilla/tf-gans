9f35ef8bfdc09d3aec789432d52af2bbe9da6db2
Nakatsuka Shunsuke <nakatsuka@cv.info.gifu-u.ac.jp>
2018-12-20 11:46:55 +0900

    [Update] add TFRecordIterator and adopt it

--- datasets/image_sampler.py
+++ datasets/image_sampler.py
@@ -1,12 +1,17 @@
+import tensorflow as tf
 from tensorflow.python.keras.preprocessing.image import Iterator
-from tensorflow.python.keras.utils import to_categorical
 import os
 import numpy as np
+from PIL import Image
 
 
 class ImageSampler:
-    def __init__(self, normalize_mode='tanh',
+    def __init__(self, target_size=None,
+                 color_mode='rgb',
+                 normalize_mode='tanh',
                  is_training=True):
+        self.target_size = target_size
+        self.color_mode = color_mode
         self.normalize_mode = normalize_mode
         self.is_training = is_training
 
@@ -17,16 +22,129 @@ class ImageSampler:
              seed=None):
         return ArrayIterator(x,
                              y,
+                             target_size=self.target_size,
+                             color_mode=self.color_mode,
                              batch_size=batch_size,
                              normalize_mode=self.normalize_mode,
                              shuffle=shuffle,
                              seed=seed,
                              is_training=self.is_training)
 
+    def flow_from_directory(self, image_dir,
+                            batch_size=32,
+                            shuffle=True,
+                            seed=None,
+                            nb_sample=None):
+        image_paths = np.array([path for path in get_image_paths(image_dir)])
+        if nb_sample is not None:
+            image_paths = image_paths[:nb_sample]
+
+        return DirectoryIterator(paths=image_paths,
+                                 target_size=self.target_size,
+                                 color_mode=self.color_mode,
+                                 batch_size=batch_size,
+                                 normalize_mode=self.normalize_mode,
+                                 shuffle=shuffle,
+                                 seed=seed,
+                                 is_training=self.is_training)
+
+    def flow_from_path(self, paths,
+                       batch_size=32,
+                       shuffle=True,
+                       seed=None,
+                       nb_sample=None):
+        if nb_sample is not None:
+            image_paths = np.array(paths[:nb_sample])
+        else:
+            image_paths = np.array(paths)
+        return DirectoryIterator(paths=image_paths,
+                                 target_size=self.target_size,
+                                 color_mode=self.color_mode,
+                                 batch_size=batch_size,
+                                 normalize_mode=self.normalize_mode,
+                                 shuffle=shuffle,
+                                 seed=seed,
+                                 is_training=self.is_training)
+
+    def flow_from_tfrecord(self, file_paths,
+                           batch_size=32,
+                           shuffle=True,
+                           seed=None):
+        return TFRecordIterator(file_paths,
+                                target_size=self.target_size,
+                                color_mode=self.color_mode,
+                                batch_size=batch_size,
+                                shuffle=shuffle,
+                                seed=seed,
+                                is_training=self.is_training)
+
+
+class DirectoryIterator(Iterator):
+    def __init__(self, paths,
+                 target_size,
+                 color_mode,
+                 batch_size,
+                 normalize_mode,
+                 shuffle,
+                 seed,
+                 is_training):
+        self.paths = paths
+        self.target_size = target_size
+        self.color_mode = color_mode
+        self.nb_sample = len(self.paths)
+        self.batch_size = batch_size
+        self.normalize_mode = normalize_mode
+        super().__init__(self.nb_sample, batch_size, shuffle, seed)
+        self.current_paths = None
+        self.is_training = is_training
+
+    def __call__(self, *args, **kwargs):
+        if self.is_training:
+            return self.flow_on_training()
+        else:
+            return self.flow_on_test()
+
+    def flow_on_training(self):
+        with self.lock:
+            index_array = next(self.index_generator)
+        image_path_batch = self.paths[index_array]
+        image_batch = np.array([load_image(path, self.target_size, self.color_mode)
+                                for path in image_path_batch])
+        self.current_paths = image_path_batch
+        return image_batch
+
+    def flow_on_test(self):
+        indexes = np.arange(self.nb_sample)
+        if self.shuffle:
+            np.random.shuffle(indexes)
+
+        steps = self.nb_sample // self.batch_size
+        if self.nb_sample % self.batch_size != 0:
+            steps += 1
+        for i in range(steps):
+            index_array = indexes[i * self.batch_size: (i + 1) * self.batch_size]
+            image_path_batch = self.paths[index_array]
+
+            image_batch = np.array([load_image(path, self.target_size, self.color_mode)
+                                    for path in image_path_batch])
+
+            self.current_paths = image_path_batch
+            yield image_batch
+
+    def data_to_image(self, x):
+        if x.shape[-1] == 1:
+            x = x.reshape(x.shape[:-1])
+        return denormalize(x, self.normalize_mode)
+
+    def __len__(self):
+        return self.nb_sample
+
 
 class ArrayIterator(Iterator):
     def __init__(self, x,
                  y,
+                 target_size,
+                 color_mode,
                  batch_size,
                  normalize_mode,
                  shuffle,
@@ -34,6 +152,8 @@ class ArrayIterator(Iterator):
                  is_training):
         self.x = x
         self.y = y
+        self.target_size = target_size
+        self.color_mode = color_mode
         self.nb_sample = len(self.x)
         self.batch_size = batch_size
         self.normalize_mode = normalize_mode
@@ -53,20 +173,19 @@ class ArrayIterator(Iterator):
     def flow_on_training(self):
         with self.lock:
             index_array = next(self.index_generator)
-        data_batch = np.array([preprocessing(x, self.normalize_mode)
-                               for x in self.x[index_array]])
-        if len(data_batch.shape) == 3:
-            data_batch = np.expand_dims(data_batch, -1)
+        image_batch = np.array([preprocessing(Image.fromarray(x),
+                                              color_mode=self.color_mode,
+                                              target_size=self.target_size)
+                                for x in self.x[index_array]])
         if self.y is not None:
             label_batch = self.y[index_array]
-            return data_batch, label_batch
+            return image_batch, label_batch
         else:
-            return data_batch
+            return image_batch
 
     def flow_on_test(self):
         indexes = np.arange(self.nb_sample)
         if self.shuffle:
-            print('Now you set is_training = False. \nBut shuffle = True')
             np.random.shuffle(indexes)
 
         steps = self.nb_sample // self.batch_size
@@ -74,52 +193,58 @@ class ArrayIterator(Iterator):
             steps += 1
         for i in range(steps):
             index_array = indexes[i * self.batch_size: (i + 1) * self.batch_size]
-            data_batch = np.array([preprocessing(x, self.normalize_mode)
-                                   for x in self.x[index_array]])
+            image_batch = np.array([preprocessing(Image.fromarray(x),
+                                                  color_mode=self.color_mode,
+                                                  target_size=self.target_size)
+                                    for x in self.x[index_array]])
             if self.y is not None:
                 label_batch = self.y[index_array]
-                yield data_batch, label_batch
+                yield image_batch, label_batch
             else:
-                yield data_batch
-
-    def random_sampling(self, batch_size=16, seed=None):
-        indexes = np.arange(self.nb_sample)
-        if seed is not None:
-            np.random.seed(seed=seed)
-        np.random.shuffle(indexes)
-
-        index_array = indexes[:batch_size]
-
-        data_batch = np.array([preprocessing(x, self.normalize_mode)
-                               for x in self.x[index_array]])
-        if len(data_batch.shape) == 3:
-            data_batch = np.expand_dims(data_batch, -1)
-
-        if self.y is not None:
-            label_batch = self.y[index_array]
-            return data_batch, label_batch
-        else:
-            return data_batch
+                yield image_batch
 
     def data_to_image(self, x):
+        if x.shape[-1] == 1:
+            x = x.reshape(x.shape[:-1])
         return denormalize(x, self.normalize_mode)
 
+    def __len__(self):
+        return self.nb_sample
 
-def preprocessing(x, normalize_mode='tanh'):
-    _x = np.asarray(x)
-    _x = normalize(_x, normalize_mode)
-    return _x
+
+def preprocessing(x, target_size=None, color_mode='rgb'):
+    assert color_mode in ['grayscale', 'gray', 'rgb']
+    if color_mode in ['grayscale', 'gray']:
+        image = x.convert('L')
+    else:
+        image = x
+
+    if target_size is not None and target_size != image.size:
+        image = image.resize(target_size, Image.BILINEAR)
+
+    image_array = np.asarray(image)
+
+    image_array = normalize(image_array)
+
+    if color_mode in ['grayscale', 'gray']:
+        image_array = image_array.reshape(image.size[1], image.size[0], 1)
+    return image_array
+
+
+def load_image(path, target_size=None, color_mode='rgb'):
+    image = Image.open(path)
+    try:
+        return preprocessing(image, target_size, color_mode)
+    except:
+        print(path)
+        exit()
 
 
 def normalize(x, mode='tanh'):
     if mode == 'tanh':
-        return (normalize(x, 'sigmoid') - 0.5) * 2.
+        return (x.astype('float32') / 255 - 0.5) / 0.5
     elif mode == 'sigmoid':
-        _x = x.astype('float32')
-        _x /= 255
-        return _x
-    elif mode is None:
-        return x
+        return x.astype('float32') / 255
     else:
         raise NotImplementedError
 
@@ -129,8 +254,6 @@ def denormalize(x, mode='tanh'):
         return ((x + 1.) / 2 * 255).astype('uint8')
     elif mode == 'sigmoid':
         return (x * 255).astype('uint8')
-    elif mode is None:
-        return x
     else:
         raise NotImplementedError
 
@@ -143,10 +266,109 @@ def get_image_paths(src_dir):
                 yield os.path.join(root, file)
 
     def is_image(path):
-        if 'png' in path or 'jpg' in path or 'bmp' in path:
+        valid_exts = ['png', 'jpg', 'jpeg', 'bmp', 'PNG', 'JPG', 'JPEG', 'BMP']
+        _, ext = os.path.splitext(path)
+        ext = ext[1:]
+        if ext in valid_exts:
             return True
         else:
             return False
 
     return [path for path in get_all_paths() if is_image(path)]
 
+
+class TFRecordIterator:
+    def __init__(self, file_paths,
+                 map_fn=None,
+                 compression_type='GZIP',
+                 target_size=None,
+                 color_mode='rgb',
+                 batch_size=32,
+                 shuffle=True,
+                 seed=None,
+                 is_training=True):
+        self.file_paths = file_paths
+        self.nb_sample = 0
+        if map_fn is not None:
+            self.map_dataset = map_fn
+        self.compression_type = compression_type
+        self._target_size = target_size
+        self.color_mode = color_mode
+        self._batch_size = batch_size
+        self.shuffle = shuffle
+        self.seed = seed
+        self.is_training = is_training
+        self._build_dataset()
+
+    def __next__(self):
+        return next(self.iterator)
+
+    def __call__(self):
+        return next(self.iterator)
+
+    def __iter__(self):
+        return self.iterator
+
+    @staticmethod
+    def data_to_image(x: tf.Tensor):
+        if x.get_shape().as_list()[-1] == 1:
+            x = x.reshape(x.get_shape().as_list()[:-1])
+        x = np.array(x)
+        return denormalize(x)
+
+    def map_dataset(self, serialized):
+        features = {
+            'height': tf.FixedLenFeature([], tf.int64),
+            'width': tf.FixedLenFeature([], tf.int64),
+            'channel': tf.FixedLenFeature([], tf.int64),
+            'image': tf.FixedLenFeature([], tf.string),
+        }
+        features = tf.parse_single_example(serialized, features)
+        height = tf.cast(features['height'], tf.int32)
+        width = tf.cast(features['width'], tf.int32)
+        channel = tf.cast(features['channel'], tf.int32)
+        images = tf.decode_raw(features['image'], tf.uint8)
+        images = tf.cast(images, tf.float32)
+        images = tf.reshape(images, (height, width, channel))
+        images = tf.image.resize_images(images, self._target_size)
+        return (images / 255 - 0.5) / 0.5
+
+    def _build_dataset(self):
+        self.dataset = tf.data.TFRecordDataset(self.file_paths, compression_type=self.compression_type)
+        self.dataset = self.dataset.repeat(1 if not self.is_training else -1)
+        self.dataset = self.dataset.map(self.map_dataset)
+        if self.shuffle:
+            self.dataset = self.dataset.shuffle(buffer_size=256, seed=self.seed)
+        self.dataset = self.dataset.batch(self._batch_size)
+        self.iterator = self.dataset.make_one_shot_iterator()
+
+    def __len__(self):
+        if self.nb_sample:
+            return self.nb_sample
+        else:
+            print('\nComputing the number of records now.'),
+            print('It takes a long time only for the 1st time\n')
+            options = tf.python_io.TFRecordOptions(
+                tf.python_io.TFRecordCompressionType.GZIP) if self.compression_type == 'GZIP' \
+                else None
+            for path in self.file_paths:
+                self.nb_sample += sum(1 for _ in tf.python_io.tf_record_iterator(path, options=options))
+        return self.nb_sample
+
+    @property
+    def target_size(self):
+        return self._target_size
+
+    @target_size.setter
+    def target_size(self, target_size):
+        self._target_size = target_size
+        self._build_dataset()
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @batch_size.setter
+    def batch_size(self, batch_size):
+        self._batch_size = batch_size
+        self._build_dataset()
