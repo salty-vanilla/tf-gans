8ba8d94388fa73dad99ba7a449b17be0b0bb593b
Nakatsuka Shunsuke <nakatsuka@cv.info.gifu-u.ac.jp>
2019-02-05 17:19:08 +0900

    [Add] impl/wgan_gp/anime

---
+++ impl/wgan_gp/anime/train.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import shutil
+import yaml
+sys.path.append(os.getcwd())
+sys.path.append('../../')
+from solver import Solver
+from datasets.image_sampler import ImageSampler
+from datasets.noise_sampler import NoiseSampler
+from models.generator import AnimeResidualGenerator as Generator
+from models.discriminator import AnimeResidualDiscriminator as Discriminator
+# from models.generator import AnimeGenerator128 as Generator
+# from models.discriminator import AnimeDiscriminator128 as Discriminator
+
+
+def main():
+    yml_path = sys.argv[1]
+    with open(yml_path) as f:
+        config = yaml.load(f)
+    os.makedirs(config['logdir'], exist_ok=True)
+    shutil.copy(yml_path, os.path.join(config['logdir'], 'config.yml'))
+
+    image_sampler = ImageSampler(target_size=(128, 128),
+                                 color_mode='rgb',
+                                 is_training=True)
+    noise_sampler = NoiseSampler('normal')
+
+    generator = Generator(**config['generator_params'])
+    discriminator = Discriminator(**config['discriminator_params'])
+
+    solver = Solver(generator,
+                    discriminator,
+                    **config['solver_params'],
+                    logdir=config['logdir'])
+
+    solver.fit_generator(image_sampler.flow_from_tfrecord(config['records'],
+                                                          batch_size=config['batch_size'],
+                                                          shuffle=True),
+                         noise_sampler,
+                         **config['fit_params'])
+
+
+if __name__ == '__main__':
+    main()
